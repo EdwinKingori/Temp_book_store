630b88a81610a88afeb9f79f29b151a6706371c9
EDDY <eddmunyiri@gmail.com>
2023-11-19 12:39:58 +0300

    configuring Admin Settings

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import Book
 # Register your models here.
+
+
+class BookAdmin(admin.ModelAdmin):
+    # readonly_fields = ('slug',)
+    # automatically generates the slug field and allows the user to edit the slug field
+    prepopulated_fields = {"slug": ('title',)}
+    list_filter = ('author', 'rating',)
+    list_display = ('title', 'author',)
+
+
+admin.site.register(Book, BookAdmin)
